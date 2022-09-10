25c9d96b51a06e4d2f3a536189c4fc31745ff22f
Khaled,El-Sayed <t0ti20@icloud.com>
2022-09-10 17:46:24 +0200

    Updationg GPIO Delete Switch Case

--- COTS/Library/Macros.h
+++ COTS/Library/Macros.h
@@ -5,11 +5,11 @@
 /* Magic_Numbers */
 typedef enum{FALSE,TRUE,ZERO=0,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHTH,NINE,TEN}Magic_Numbers;
 /* Bit Math */
-#define Set_Port(Reg,Bit)   ((Reg)  =  (Bit))					/* Set Bit */
-#define Clear_Port(Reg) 	((Reg)  =  (0x0))					/* Set Bit */
+#define Set_Port(Reg,Bit)   ((Reg)  =  (Bit))					/* Set Port */
+#define Clear_Port(Reg) 	((Reg)  =  (0x0))					/* Clear Port */
 #define Set_Bit(Reg,Bit)    ((Reg) |=  (Bit))					/* Set Bit */
 #define Clear_Bit(Reg,Bit)  ((Reg) &= ~(Bit))					/* Clear Bit */
-#define Get_Bit(Reg,Bit)    ((Reg)  &  (Bit))					/* Get_Bit */
+#define Get_Bit(Reg,Bit)    ((Reg)  &  (Bit))					/* Get Bit */
 /*-------------- File Guard ------------------*/
 #endif
 _MACROS_H_
--- COTS/MCAL/GPIO/GPIO_Config.h
+++ COTS/MCAL/GPIO/GPIO_Config.h
@@ -2,6 +2,6 @@
 #ifndef _GPIO_CONFIG_H_
 #define _GPIO_CONFIG_H_
 /********************  GPIO CONFIGURATIONS  ***************/
-#define Open_Drain_Pull			Pull_Up
+#define Open_Drain_Pull			Pull_Up		/*Open Drain Configure (Pull_Up-Pull_Down)*/
 #endif
 /* _GPIO_CONFIG_H_ */
